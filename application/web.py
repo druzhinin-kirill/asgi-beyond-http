550c9b54c6a088edb3b6c5755e4bdf45854c3812
Kirill Druzhinin <kirill.druzhinin@teampicnic.com>
2025-06-24 23:03:28 +0200

    Create UI

--- application/web.py
+++ application/web.py
@@ -15,10 +15,29 @@ MAP_CENTER = (52.50, 13.350)
 
 @router.page("/")
 async def index():
-    m = ui.leaflet(center=MAP_CENTER)
-    marker = m.marker(latlng=m.center)
+    ui.label.default_classes("text-gray-900")
+    ui.card.default_classes("col text-center")
+    ui.card.default_props("flat bordered")
 
-    logs = ui.log()
+    with ui.grid(rows="1fr 3fr").classes("w-11/12 place-self-center h-screen"):
+        with ui.column(align_items="center").classes(
+            "mt-4 text-center w-5/6 place-self-center"
+        ):
+            ui.label("Breaking HTTP Boundaries").classes("text-h2 font-bold")
+            ui.label("ASGI to build a fleet management system in Python").classes(
+                "mb-4 text-h4"
+            )
+
+        with ui.row(align_items="stretch").classes("mb-4"):
+            with ui.card(align_items="center"):
+                ui.label("Map").classes("text-h4")
+                m = ui.leaflet(center=MAP_CENTER).classes("h-full container")
+                marker = m.marker(latlng=m.center)
+
+            with ui.card(align_items="center"):
+                ui.label("Live Logs").classes("text-h4")
+                logs = ui.log().classes("h-96 container")
+
     ui.timer(1.0, lambda: marker.move(vehicle.lat, vehicle.lng))
 
     async def show_logs():
